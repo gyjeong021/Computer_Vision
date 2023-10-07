5c2933fdd846e20abeec1b0777aca7126efc5058
jiyoung <gyjeong021@gmail.com>
2023-10-07 23:33:56 +0900

    9/21 수업

--- ch3/3-2.py
+++ ch3/3-2.py
@@ -1,6 +1,17 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-img=cv.imread('soccer.jpg') 
+img=cv.imread('soccer.jpg')
+
+# opencv에서는 컬러가 b, g, r 순으로 저장
+# calcHist에서 반환되는 값은 각각 색깔에 대한 카운트 값
 h=cv.calcHist([img],[2],None,[256],[0,256]) # 2번 채널인 R 채널에서 히스토그램 구함
-plt.plot(h,color='r',linewidth=1)
+plt.plot(h,color='r',linewidth=1) # linewidth : 선 두께
+
+hg=cv.calcHist([img],[1],None,[256],[0,256]) # 1번 채널인 G 채널에서 히스토그램 구함
+plt.plot(hg,color='g',linewidth=2, linestyle="dotted")
+
+hb=cv.calcHist([img],[0],None,[256],[0,256]) # 번 채널인 B 채널에서 히스토그램 구함
+plt.plot(hb,color='b',linewidth=3, linestyle="dashed")
+
+plt.show()
--- ch3/3-3.py
+++ ch3/3-3.py
@@ -1,13 +1,18 @@
 import cv2 as cv
-import sys
 
-img=cv.imread('soccer.jpg') 
-            
-t,bin_img=cv.threshold(img[:,:,2],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+# img=cv.imread('soccer.jpg')
+gray=cv.imread('soccer.jpg', cv.IMREAD_GRAYSCALE)
+
+# 반환값 : 오츄 알고리즘으로 찾은 임계값, 이진화 시킨 결과
+# thresh에는 임의의 값 넣어도 괜찮
+# t,bin_img=cv.threshold(img[:,:,2],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+t,bin_img=cv.threshold(gray,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 print('오츄 알고리즘이 찾은 최적 임곗값=',t)
 
-cv.imshow('R channel',img[:,:,2])			# R 채널 영상
-cv.imshow('R channel binarization',bin_img)	# R 채널 이진화 영상
+#cv.imshow('R channel',img[:,:,2]) # R 채널 영상
+cv.imshow('Gray', gray) # gray 영상
+# cv.imshow('R channel binarization',bin_img) # R 채널 이진화 영상
+cv.imshow('Gray binarization',bin_img)
 
 cv.waitKey()
 cv.destroyAllWindows()
