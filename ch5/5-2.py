6d615d516c35651355ab3e90ef59be2705940d6e
jiyoung <gyjeong021@gmail.com>
2023-12-06 18:34:57 +0900

    SIFT 기술자와 매칭 전략

--- ch5/5-2.py
+++ ch5/5-2.py
@@ -1,18 +1,19 @@
 import cv2 as cv
 
 img=cv.imread('mot_color70.jpg') # 영상 읽기
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY) # 특징 검출 알고리즘은 대부분 입력 영상이 gray
 
-sift=cv.SIFT_create() 
+sift=cv.SIFT_create() # sift 계산할 수 있는 객체 생성
 kp,des=sift.detectAndCompute(gray,None)
 # sift.detectAndCompute(inputImg,mask=None)
-# 특징점 검출과 특징 디스크립터 계산을 한 번에 수행
+# 특징점 검출과 기술자 계산을 한 번에 수행
+# kp = keypoint(해당되는 특징점의 위치), des = descriptor(그것을 기술한 것)
 # 2mask : 특징점 검출에 사용할 필터
 print(len(kp))
-print(kp[0].pt, kp[0].size, kp[0].octave, kp[0].angle)
-print(des[0])
+print(kp[0].pt, kp[0].size, kp[0].octave, kp[0].angle) # 위치, 사이즈, 옥타브, 각도
+print(des[0]) # 총 128개의 값 기술, 16개 block에 대한 히스토그램 값
 
-# gray=cv.drawKeypoints(gray,kp,None,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) # 크기, 회전에 대한 정보도 포함
+# gray=cv.drawKeypoints(gray,kp,None,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) # 스케일에 의해 검출된 크기, 각도에 대한 정보 포함
 gray=cv.drawKeypoints(gray,kp,None,flags=cv.DRAW_MATCHES_FLAGS_DEFAULT) # 특징 위치만 표시
 # drawKeypoints(inputImg,kp,outImg=None,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 # flags : cv.DRAW_MATCHES_FLAGS_DEFAULT, cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINT, cv.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG
--- ch5/5-3.py
+++ ch5/5-3.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 import time
 
+# 2개의 이미지를 가져와 gray로 변환
 img1=cv.imread('mot_color70.jpg')[190:350,440:560] # 버스를 크롭하여 모델 영상으로 사용
 gray1=cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
 img2=cv.imread('mot_color83.jpg')			     # 장면 영상
@@ -20,21 +21,26 @@ start=time.time()
 # knn_match=bf_matcher.knnMatch(des1,des2,2)
 # knnMatch는 가장 유사한 특징점 k(=2)개를 결과로 받음
 
+# 앞에서 계산한 기술자들을 매칭해주는 객체 생성
+# 전수조사(Brute-Force)가 아닌 FLANN 방식 사용
 flann_matcher=cv.DescriptorMatcher_create(cv.DescriptorMatcher_FLANNBASED)
-knn_match=flann_matcher.knnMatch(des1,des2,2) # k = 2 # FLANN 방법 사용
+knn_match=flann_matcher.knnMatch(des1,des2,2) # k = 2, 가장 가까운 값, 두번째로 가까운 값
 print(len(knn_match))
 
 T=0.7
 good_match=[]
 for nearest1,nearest2 in knn_match:
-    if (nearest1.distance/nearest2.distance)<T:
+    if (nearest1.distance/nearest2.distance)<T: # 매칭 전략 : 최근접 이웃 거리 비율
         good_match.append(nearest1)
 print(len(good_match))
 print(good_match[0].queryIdx, ' -- ', good_match[0].trainIdx, ' : ', good_match[0].distance)
 print('매칭에 걸린 시간:',time.time()-start) 
 
-img_match=np.empty((max(img1.shape[0],img2.shape[0]),img1.shape[1]+img2.shape[1],3),dtype=np.uint8) # y 값, w 값, color
+img_match=np.empty((max(img1.shape[0],img2.shape[0]),img1.shape[1]+img2.shape[1],3),dtype=np.uint8) # 배열을 만듦, 두개의 이미지를 하나의 이미지로 만들기 위해
+# 두개의 이미지 중 큰 y 값, 두개의 이미지 더한 x 값, color
 cv.drawMatches(img1,kp1,img2,kp2,good_match,img_match,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# drawMatches(첫번째 이미지, 첫번째 keypoint, 두번째 이미지, 두번째 keypoint, 둘 사이에서 찾은 매칭, output 이미지
+# -> 매칭된 것 직선으로 연결해줌
 
 cv.imshow('Good Matches', img_match)
 
